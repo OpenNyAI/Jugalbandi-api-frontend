ea8289c27615e2ebeb8804cda93b4492991ceded
prochetab1999 <procheta.b@thoughtworks.com>
2023-04-21 16:13:43 +0530

    [Procheta |  Priya] Add. pdf formatter

--- src/components/PdfModal.js
+++ src/components/PdfModal.js
@@ -1,9 +1,10 @@
 import { Modal } from 'antd';
 import React from 'react';
+import Pdf from './Pdf';
 
 const PdfModal = ({ IsPdfModalOpen, handleCancel, pdfContent }) => (
   <Modal title="Basic Modal" open={IsPdfModalOpen} onCancel={handleCancel} footer={null}>
-    <p>{pdfContent}</p>
+    <Pdf pdfContent={pdfContent} />
   </Modal>
 );
 
