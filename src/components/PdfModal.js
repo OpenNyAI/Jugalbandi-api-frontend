07238d33cd7451ad268c0fada43db618a00f75a9
prochetab1999 <procheta.b@thoughtworks.com>
2023-04-21 18:14:11 +0530

    [Procheta |  Priya] Refactor. pdf formatter

--- src/components/PdfModal.js
+++ src/components/PdfModal.js
@@ -1,10 +1,13 @@
 import { Modal } from 'antd';
 import React from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
 import Pdf from './Pdf';
 
 const PdfModal = ({ IsPdfModalOpen, handleCancel, pdfContent }) => (
   <Modal title="Basic Modal" open={IsPdfModalOpen} onCancel={handleCancel} footer={null}>
-    <Pdf pdfContent={pdfContent} />
+    <PDFViewer width="100%" height="1000px">
+      <Pdf pdfContent={pdfContent} />
+    </PDFViewer>
   </Modal>
 );
 
