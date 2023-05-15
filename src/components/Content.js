58ff6d377f4edfc3beed7ca02cb72f8eba320432
prochetab1999 <procheta.b@thoughtworks.com>
2023-05-15 17:54:32 +0530

    [Procheta] Add. content and highlight and margins

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-debugger */
 import React from 'react';
 
@@ -11,29 +12,34 @@ const Content = ({ contentList, highlightedPortions }) => {
   + sortedArr[sortedArr.length - 1].length;
   // console.log('segments', contentList.substring(startingRange, endingRange));
   sortedArr.forEach((highlightedPortion) => {
-    const highlights = highlightedPortion.split('\n').filter((str) => str !== ' ');
-    highlights.forEach((highlight) => highlightedArray.push(highlight.trim()));
+    const highlights = highlightedPortion.replaceAll('\\n \\n', '\n \n').split('\n \n').filter((str) => str !== ' ' && str !== '  ');
+    highlights.forEach((highlight) => highlightedArray.push(highlight.trim().replaceAll('\\n', '')));
   });
-  const segments = contentList?.split('\n').filter((str) => str !== ' ');
+  const highlightsArray = highlightedArray.filter((trimmedSegment) => trimmedSegment !== '');
+  console.log('highlights 1', highlightsArray);
+  const segments = contentList?.replaceAll('\\n \\n', '\n \n').split('\n \n').filter((str) => str !== ' ' && str !== '  ');
+  // console.log('highlight 2', contentList.replaceAll('\\n \\n', '\n \n').
+  // split('\n \n').filter((str) => str !== ' ' && str !== '  '));
   const trimmedSegments = [];
   segments.forEach((segment) => {
-    trimmedSegments.push(segment.trim());
+    trimmedSegments.push(segment.trim().replaceAll('\\n', ''));
   });
   // console.log('highlights 5', startingRange, endingRange);
   // console.log(segments);
-  console.log('trims', trimmedSegments);
-  const highlightedSegments = trimmedSegments.map((segment) => {
+  const trimmedArray = trimmedSegments.filter((trimmedSegment) => trimmedSegment !== '');
+  // console.log('highlight 3', trimmedArray);
+  const highlightedSegments = trimmedArray.map((segment) => {
     // const highlighted = highlightedArray.find((highlight) => segment.includes(highlight));
     let highlighted;
     let intermediate = true;
     let startingHighlightRange = false;
     const positions = [];
     // console.log('index 1', segment);
-    highlightedArray.forEach((highlight) => {
+    highlightsArray.forEach((highlight) => {
       // console.log('index 2', highlight);
       // console.log('index 3', contentList.indexOf(segment), ' ', segment.includes(highlight));
-      if ((contentList.indexOf(segment) >= startingRange
-      && contentList.indexOf(segment) <= endingRange)) {
+      if ((contentList.indexOf(segment) + segment.indexOf(highlight) >= startingRange
+      && contentList.indexOf(segment) + segment.indexOf(highlight) <= endingRange)) {
         if (segment.includes(highlight)) {
           highlighted = segment.includes(highlight);
           const startingIndex = segment.indexOf(highlight);
@@ -78,17 +84,11 @@ const Content = ({ contentList, highlightedPortions }) => {
         break;
       }
     }
-    for (let i = 0; i < positions.length; i += 1) {
-      if (positions[i].start === 0) {
-        startingHighlightRange = true;
-        break;
-      }
-    }
-    if (positions.length > 0) {
-      console.log('highlights 45', positions);
-    }
+    // if (positions.length > 0) {
+    //   console.log('highlights 45', positions);
+    // }
     return highlighted ? (
-      <div>
+      <div style={{ marginBottom: '10px' }}>
         {!startingHighlightRange ? <span>{segment.substring(0, positions[0].start)}</span> : <span style={{ backgroundColor: 'yellow' }}>{segment.substring(0, positions[0].start)}</span>}
 
         {positions.map((pos, index) => (
@@ -116,7 +116,7 @@ const Content = ({ contentList, highlightedPortions }) => {
 
       </div>
     ) : (
-      <div>{segment}</div>
+      <div style={{ marginBottom: '10px' }}>{segment}</div>
     );
   });
   return (
