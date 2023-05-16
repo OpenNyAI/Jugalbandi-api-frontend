975ba45960e347ac35699e71917c9812a07558bd
prochetab1999 <procheta.b@thoughtworks.com>
2023-05-16 13:49:52 +0530

    [Procheta] Refactor. feedback mechanism

--- src/utlities/Chatbot.js
+++ src/utlities/Chatbot.js
@@ -10,5 +10,15 @@ const getChatbotResponse = async (uuid_number, query_string) => {
   const response = await Api.get(url, params);
   return response;
 };
+const sendFeedback = async (feedback, queryAnswer, uuid_number) => {
+  const url = 'https://api.jugalbandi.ai/response-feedback';
+  const response = await Api.post(url, {
+    uuid_number,
+    query: queryAnswer.query,
+    response: queryAnswer.response,
+    feedback,
+  });
+  return response;
+};
 
-export default { getChatbotResponse };
+export default { getChatbotResponse, sendFeedback };
