e764f5349a8c074581adb255fa61574ccf36df08
Saurabh Karn <karnsaurabhkumar@gmail.com>
2023-08-09 16:24:56 +0530

    Update UuidDatabase.js
    Added Data Protection Bill 2023

--- src/UuidDatabase.js
+++ src/UuidDatabase.js
@@ -1,5 +1,9 @@
 const uuidDatabase = [
   {
+    value: '03f67edc-36a2-11ee-8d88-e5d7df1d6890',
+    label: 'Digital Personal Data Protection Bill, 2023',
+  },
+  {
     value: 'bb01d73a-ea52-11ed-9bb7-378d76577400',
     label: 'The Karnataka State Civil Services (Regulation of Promotion, Pay and Pension) Act, 1973 (bb01d73a-ea52-11ed-9bb7-378d76577400)',
   },
@@ -14,10 +18,6 @@ const uuidDatabase = [
   {
     value: '2ea8058c-f3be-11ed-866b-3e85235234ac',
     label: 'The Karnataka Agricultural Pests And Diseases Act, 1968 (2ea8058c-f3be-11ed-866b-3e85235234ac)',
-  },
-  {
-    value: '5ba9c080-1bb8-11ee-aada-65a812796123',
-    label: 'The Digital Personal Data Protection Bill, 2022 (5ba9c080-1bb8-11ee-aada-65a812796123)',
   },
 ];
 export default uuidDatabase;
